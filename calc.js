d597d4a411e86edb27cd99f36d9e6608482dd413
Justin <justingraham1108@gmail.com>
2023-09-08 09:34:46 +0100

    initial up

---
+++ calc.js
@@ -0,0 +1,15 @@
+var colors = require('ansi-colors');
+var prompt = require('prompt-sync');
+//     history: require('prompt-sync-history')(),
+//     autocomplete: complete(['hello1234', 'he', 'hello', 'hello12', 'hello123456']),
+//     sigint: false
+//   });
+
+  console.log(require('prompt-sync')()('tell me something about yourself: '))
+
+function calc (a, b, c)   {
+}
+
+var value = 'justin';
+var name = prompt('enter name: ', value);
+
